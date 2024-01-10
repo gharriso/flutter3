8c2d644ded35d7ac8cb0bc444cfc31b539e3bdc3
Guy Harrison <guy.a.harrison@gmail.com>
2024-01-10 18:03:19 +1100

    WIP - turbo 3.5 working somewhat

--- namer_app/lib/main.dart
+++ namer_app/lib/main.dart
@@ -1,114 +1,95 @@
-import 'package:english_words/english_words.dart';
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+// ignore_for_file: use_build_context_synchronously
 
-void main() {
-  runApp(MyApp());
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+import 'dart:io';
+
+Future<String> processPrompt(String prompt) async {
+  var url = Uri.parse('https://api.openai.com/v1/completions');
+  // Combine two strings
+  var openAIKey = Platform.environment['OPENAI_KEY'];
+  var authKey = 'Bearer ${openAIKey}';
+  print(authKey);
+  var headers = {'Content-Type': 'application/json', 'Authorization': authKey};
+  var body = convert.jsonEncode(
+      {'prompt': prompt, "model": "gpt-3.5-turbo-instruct", 'max_tokens': 128});
+
+  var response = await http.post(url, headers: headers, body: body);
+
+  if (response.statusCode == 200) {
+    var jsonResponse = convert.jsonDecode(response.body);
+    print(jsonResponse['choices'][0]['text']);
+    return jsonResponse['choices'][0]['text']['content'];
+  } else {
+    print('Request failed with status: ${response.statusCode}.');
+    print(convert.json.decode(response.body));
+    return response.statusCode.toString();
+  }
 }
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'Namer App',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
-        ),
-        home: MyHomePage(),
-      ),
+    return const MaterialApp(
+      title: 'AI Helper',
+      home: MyCustomForm(),
     );
   }
 }
 
-class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
-  void getNext() {
-    current = WordPair.random();
-    notifyListeners();
-  }
+// Define a custom Form widget.
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({super.key});
 
-  var favorites = <WordPair>[];
-    void toggleFavorite() {
-    if (favorites.contains(current)) {
-      favorites.remove(current);
-    } else {
-      favorites.add(current);
-    }
-    notifyListeners();
-  }
+  @override
+  State<MyCustomForm> createState() => _MyCustomFormState();
 }
 
-class MyHomePage extends StatelessWidget {
+// Define a corresponding State class.
+// This class holds the data related to the Form.
+class _MyCustomFormState extends State<MyCustomForm> {
+  // Create a text controller and use it to retrieve the current value
+  // of the TextField.
+  final myController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    myController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var pair = appState.current;
-
-    IconData icon;
-    if (appState.favorites.contains(pair)) {
-      icon = Icons.favorite;
-    } else {
-      icon = Icons.favorite_border;
-    }
-
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            bigCard(pair: pair),
-            SizedBox(height: 10),
-            Row(mainAxisSize: MainAxisSize.min, children: [
-              ElevatedButton.icon(
-                onPressed: () {
-                  appState.getNext();
-                },
-                icon: Icon(icon),
-                label: Text('Like'),
-              ),
-              SizedBox(width: 10),
-              ElevatedButton(
-                onPressed: () {
-                  appState.getNext();
-                },
-                child: Text('Next'),
-              ),
-            ]),
-          ],
+      appBar: AppBar(
+        title: const Text('Question?'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: TextField(
+          controller: myController,
         ),
       ),
-    );
-  }
-}
-
-class bigCard extends StatelessWidget {
-  const bigCard({
-    super.key,
-    required this.pair,
-  });
-
-  final WordPair pair;
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final style = theme.textTheme.displayMedium!.copyWith(
-      color: theme.colorScheme.onPrimary,
-    );
-    return Card(
-      color: theme.colorScheme.primary,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Text(
-          pair.asCamelCase,
-          style: style,
-          semanticsLabel: "${pair.first} ${pair.second}",
-        ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          String result = await processPrompt('Say hello');
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                content: Text(result),
+              );
+            },
+          );
+        },
+        tooltip: 'Execute',
+        child: const Icon(Icons.text_fields),
       ),
     );
   }
